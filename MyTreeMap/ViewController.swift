ba7227448d8ae001bcf4ad4dba2d0cba040d3e23
Maximilian Rogers <max@carrotcreative.com>
2016-07-26 15:24:34 -0400

    Initial Commit

---
+++ MyTreeMap/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyTreeMap
+//
+//  Created by Max Rogers on 7/26/16.
+//  Copyright © 2016 max rogers. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
