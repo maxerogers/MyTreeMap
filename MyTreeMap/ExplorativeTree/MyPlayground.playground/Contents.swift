8144f6fc95ff16c1a60355d1cc95e1e4540bb15f
Maximilian Rogers <max@carrotcreative.com>
2016-07-26 18:13:32 -0400

    add Btree

--- MyTreeMap/ExplorativeTree/MyPlayground.playground/Contents.swift
+++ MyTreeMap/ExplorativeTree/MyPlayground.playground/Contents.swift
@@ -2,19 +2,50 @@
 
 import Cocoa
 
+struct File : Hashable {
+    let size: Int
+    let path: String
+    let name: String
+    init(name: String, path: String, size: Int) {
+        self.name = name
+        self.path = path
+        self.size = size
+    }
+    var hashValue: Int { get { return "\(path)/\(name)".hashValue } }
+}
+
+func ==(lhs:File, rhs:File) -> Bool {
+    return lhs.hashValue == rhs.hashValue
+}
+
+class Node<T: Hashable> : Hashable {
+    var parent: Node?
+    var children: [Node] = []
+    var value: T
+    init(_ value: T) {
+        self.value = value
+    }
+    var hashValue: Int { get { return value.hashValue } }
+}
+
+func ==<T>(lhs:Node<T>, rhs:Node<T>) -> Bool {
+    return lhs.hashValue == rhs.hashValue
+}
+
 var str = "Hello, playground"
+
 let fileManager = NSFileManager()
 //var documentsPath = NSSearchPathForDirectoriesInDomains(.UserDirectory,
 //                                                        .AllDomainsMask,
 //                                                        true)
 let path = "/Users/maxrogers/dev/MyTreeMap/MyTreeMap/ExplorativeTree"
 let contents = try fileManager.contentsOfDirectoryAtPath(path)
-for document in contents {
-    print(document)
-    do {
-        let attrs = try fileManager.attributesOfItemAtPath("/Users/maxrogers/dev/MyTreeMap/MyTreeMap/ExplorativeTree")
 
-        print(attrs["NSFileSize"]!)
+for document in contents {
+    do {
+        let attrs = try fileManager.attributesOfItemAtPath("\(path)/\(document)")
+        print("\(document) : \(attrs["NSFileType"]!) : \(attrs["NSFileSize"]!) bytes")
+        
     }
     catch { print(error) }
 }
